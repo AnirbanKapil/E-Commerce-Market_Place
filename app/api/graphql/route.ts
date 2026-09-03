f3224e52d3e5f97c2bf70313548bbc0bf5210dc5
Anirban Kapil <anirbankapil@gmail.com>
2026-09-03 10:28:53 +0530

    chore: update dependencies in package.json
    - Added @apollo/server, @as-integrations/next, axios, and graphql
    - Removed duplicate prisma entry

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,55 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import prisma from "@/lib/prisma";
+import { NextRequest } from 'next/server';
+
+
+const typeDefs = `#graphql
+  type User {
+    id: Int!
+    email: String!
+    name: String
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: Int!): User
+  }
+
+  type Mutation {
+    createUser(email: String!, name: String): User!
+  }
+`
+
+const resolvers = {
+    Query: {
+    users: async () => {
+      return await prisma.user.findMany()
+    },
+    user: async (_: any, { id }: { id: number }) => {
+      return await prisma.user.findUnique({ where: { id } })
+    },
+  },
+    Mutation: {
+    createUser: async (_: any, { email, name }: { email: string; name?: string }) => {
+      return await prisma.user.create({
+        data: { email, name },
+      })
+    },
+  },
+};
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+    context : async(req) => ({req})
+});
+
+
+
+export { handler as GET, handler as POST };
