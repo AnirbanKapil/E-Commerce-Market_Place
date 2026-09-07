b398df2f0c5595322c93ccca073ec4284b5a1c25
Anirban Kapil <anirbankapil@gmail.com>
2026-09-07 10:39:27 +0530

    feat: update GraphQL schema to include user registration and session management

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -2,54 +2,76 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import prisma from "@/lib/prisma";
 import { NextRequest } from 'next/server';
-
+import { authOptions } from "../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next"
 
 const typeDefs = `#graphql
   type User {
-    id: Int!
+    id: String!
     email: String!
     name: String
+    username: String
   }
 
   type Query {
+    me: User
     users: [User!]!
-    user(id: Int!): User
   }
 
   type Mutation {
-    createUser(email: String!, name: String): User!
+    register(email: String!, username: String!, name: String!, password: String!): User!
   }
 `
 
 const resolvers = {
-    Query: {
-    users: async () => {
-      return await prisma.user.findMany()
-    },
-    user: async (_: any, { id }: { id: number }) => {
-      return await prisma.user.findUnique({ where: { id } })
-    },
-  },
-    Mutation: {
-    createUser: async (_: any, { email, name, password }: { email: string, name?: string, password: string }) => {
-      return await prisma.user.create({
-        data: { email, name, password},
+  Query: {
+    users: async () => await prisma.user.findMany(),
+    me: async (_: any, __: any, context: any) => {
+    
+      if (!context.session?.user) {
+        throw new Error("Not authenticated")
+      }
+      return await prisma.user.findUnique({
+        where: { email: context.session.user.email }
       })
-    },
+    }
   },
-};
+  Mutation: {
+    register: async (_: any, { email, username, name, password }: any) => {
+      const existingUser = await prisma.user.findFirst({
+        where: { OR: [{ email }, { username }] }
+      })
 
+      if (existingUser) {
+        throw new Error("User with this email or username already exists")
+      }
+
+      
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      return await prisma.user.create({
+        data: {
+          email,
+          username,
+          name,
+          password: hashedPassword,
+        }
+      })
+    }
+  }
+}
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
-
+  typeDefs,
+  resolvers,
+})
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
-    context : async(req) => ({req})
-});
+  context: async (req) => {
+  
+    const session = await getServerSession(authOptions)
+    return { req, session }
+  },
+})
 
-
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
