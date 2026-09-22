8ed6c9ef38682103bfd34bd44b9c69012a383e07
Anirban Kapil <anirbankapil@gmail.com>
2026-09-23 00:05:19 +0530

    feat: add RegisterForm component and create registration page

---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from '@/app/components/RegisterForm'
+
+export default function RegisterPage() {
+  return (
+    <main className="flex items-center justify-center min-h-[calc(100vh-70px)] bg-gray-50 px-4">
+      <RegisterForm />
+    </main>
+  )
+}
